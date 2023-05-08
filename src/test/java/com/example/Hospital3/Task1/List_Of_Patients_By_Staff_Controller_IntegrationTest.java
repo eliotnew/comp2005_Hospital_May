f33a63b818833a969417771fef076166c56db6fd
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-05-08 22:30:55 +0100

    Task 1 has an integration test that isnt quite building at the dependancy injection properly

--- src/test/java/com/example/Hospital3/Task1/List_Of_Patients_By_Staff_Controller_IntegrationTest.java
+++ src/test/java/com/example/Hospital3/Task1/List_Of_Patients_By_Staff_Controller_IntegrationTest.java
@@ -1,7 +1,62 @@
 package com.example.Hospital3.Task1;
 
+import Models.Allocation;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class List_Of_Patients_By_Staff_Controller_IntegrationTest {
+
+
+    private GetAllocations mockGetAllocations = mock(GetAllocations.class);
+    private CreateAdmissionIDList realCreateAdmissionIDList = new CreateAdmissionIDList();
+    private GetPatientIDWithAdmissionID mockGetStaffsPatients = mock(GetPatientIDWithAdmissionID.class);
+    private RemoveDuplicates realRemove = new RemoveDuplicates();
+    List_Of_Patients_By_Staff_Controller realController;
+
+
+
+    @Before
+    public void setup(){
+
+       realController = new List_Of_Patients_By_Staff_Controller(mockGetAllocations,realCreateAdmissionIDList,mockGetStaffsPatients,realRemove);
+
+
+        Allocation[] testAllocations = new Allocation[1];
+
+        Allocation allocation1 = new Allocation();
+        allocation1.setId(43);
+        allocation1.setAdmissionID(121);
+        allocation1.setEmployeeID(40);
+        allocation1.setStartTime("2020-11-28T16:45:00.000Z");
+        allocation1.setEndTime("2020-11-28T17:45:00.000Z");
+
+        testAllocations[0] = allocation1;
+
+        when(mockGetAllocations.getAllocations()).thenReturn(testAllocations); //does not require a running database and api
+
+
+        List<Integer> mockPatientIDList = new ArrayList<>();
+        mockPatientIDList.add(72);
+
+
+        when(mockGetStaffsPatients.getPatientIDWithAdmissionID(any())).thenReturn(mockPatientIDList); //saves doing an api get
+
+    }
+
+    @Test
+    void IntegrateTestThatListCorrectlyReturned(){
+        List<Integer> result = realController.Get_Staffs_Patients(40); //takes list of admission ids
+        assertEquals(1,result.size());
+    }
+
+
 
 }
